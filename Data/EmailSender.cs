ce301bd111918f0065f96e3005ff991147ac8608
Michael Washington <webmaster@adefwebserver.com>
2020-03-15 08:56:34 -0700

    Sending emails on new Ticket working

--- Data/EmailSender.cs
+++ Data/EmailSender.cs
@@ -1,37 +1,94 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SyncfusionHelpDesk
 {
     public class EmailSender
     {
         private readonly IConfiguration configuration;
+        private readonly IHttpContextAccessor httpContextAccessor;
 
-        public EmailSender(IConfiguration Configuration)
+        public EmailSender(
+            IConfiguration Configuration,
+            IHttpContextAccessor HttpContextAccessor)
         {
             configuration = Configuration;
+            httpContextAccessor = HttpContextAccessor;
         }
 
-        public async Task SendEmail(string EmailAddress, string EmailSubject)
+        public async Task SendEmail(string EmailType, string EmailAddress, string TicketGuid)
         {
-            var apiKey = configuration["SENDGRID_APIKEY"];
-            var senderEmail = configuration["SenderEmail"];
-            var client = new SendGridClient(apiKey);
-            var msg = new SendGridMessage()
+            try
             {
-                From = new EmailAddress(senderEmail, senderEmail),
-                Subject = EmailSubject,
-                PlainTextContent = "Hello, Email!",
-                HtmlContent = "<strong>Hello, Email!</strong>"
-            };
-            msg.AddTo(new EmailAddress(EmailAddress, EmailSubject));
-            var response = await client.SendEmailAsync(msg);
+                SendGridMessage msg = new SendGridMessage();
+                var apiKey = configuration["SENDGRID_APIKEY"];
+                var senderEmail = configuration["SenderEmail"];
+                var client = new SendGridClient(apiKey);
+                var FromEmail = new EmailAddress(senderEmail, senderEmail);
+
+                // Format Email contents
+                string strPlainTextContent = $"{EmailType}: {GetHelpDeskTicketUrl(TicketGuid)}";
+                string strHtmlContent = $"<b>{EmailType}:</b> ";
+                strHtmlContent = strHtmlContent + $"<a href='{ GetHelpDeskTicketUrl(TicketGuid) }'>";
+                strHtmlContent = strHtmlContent + $"{GetHelpDeskTicketUrl(TicketGuid)}</a>";
+
+                if (EmailType == "Help Desk Ticket Created")
+                {
+                    msg = new SendGridMessage()
+                    {
+                        From = FromEmail,
+                        Subject = EmailType,
+                        PlainTextContent = strPlainTextContent,
+                        HtmlContent = strHtmlContent
+                    };
+
+                    // Created Email always goes to Administrator
+                    // Send to senderEmail configured in appsettings.json
+                    msg.AddTo(new EmailAddress(senderEmail, EmailType));
+                }
+
+                if (EmailType == "Help Desk Ticket Updated")
+                {
+                    msg = new SendGridMessage()
+                    {
+                        From = FromEmail,
+                        Subject = EmailType,
+                        PlainTextContent = strPlainTextContent,
+                        HtmlContent = strHtmlContent
+                    };
+
+                    // Updated emails go to Administrator or Ticket creator
+                    // Send to EmailAddress passed to method
+                    msg.AddTo(new EmailAddress(EmailAddress, EmailType));
+                }
+
+                var response = await client.SendEmailAsync(msg);
+            }
+            catch
+            {
+                // Could not send email
+                // Perhaps SENDGRID_APIKEY not set in 
+                // appsettings.json
+            }
         }
+
+        // Utility
+
+        #region public string GetHelpDeskTicketUrl(string TicketGuid)
+        public string GetHelpDeskTicketUrl(string TicketGuid)
+        {
+            var request = httpContextAccessor.HttpContext.Request;
+
+            var host = request.Host.ToUriComponent();
+
+            var pathBase = request.PathBase.ToUriComponent();
+
+            return $@"{request.Scheme}://{host}{pathBase}/emailticketedit/{TicketGuid}";
+        }
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -6,7 +6,15 @@ Sample Code for the Syncfusion Help Desk application featured in the Blazor Succ
 
 ### To Install
 
-1) Create Database and run scripts in !SQL directory
+1) Create a Database on your SQL server, and run scripts in **!SQL directory**
 2) Edit *appsettings.json* to set the database connection in the DefaultConnection property
-3) Run the application, click the *Register* link and create a user named Admin@BlazorHelpWebsite.com
-4) Log out and log back in as Admin@BlazorHelpWebsite.com. You will now be the Administrator 
+3) Run the application, click the *Register* link and create a user named **Administrator@Email**
+4) Log out and log back in as **Administrator@Email**. You will now be the Administrator 
+
+
+### To Enable Emails
+
+1) Get an API key from [app.sendgrid.com](https://app.sendgrid.com)
+2) Open **appsettings.json** 
+- Uncomment the '//' before **SENDGRID_APIKEY** and enter your **SendGrid API key** in place of: **{{ uncomment and enter your key from app.sendgrid.com }}**
+- Uncomment the '//' before **SenderEmail** and enter your Email address: **{{ uncomment and enter your email address }}**
