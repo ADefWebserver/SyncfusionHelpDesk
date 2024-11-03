c8c5a5f22441aff546d95544e97564eeb16c4255
Michael Washington <webmaster@adefwebserver.com>
2024-11-03 07:11:03 -0800

    Validation working

---
+++ Models/HelpDeskTicketDTO.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
+namespace SyncfusionHelpDesk.Models
+{
+    public class HelpDeskTicketDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TicketStatus { get; set; }
+
+        [Required]
+        public DateTime TicketDate { get; set; }
+
+        [Required(ErrorMessage = "The Ticket Description field is required.")]
+        [StringLength(50, MinimumLength = 2,
+        ErrorMessage = "Description must be a minimum of 2 and maximum of 50 characters.")]
+        public string TicketDescription { get; set; }
+
+        [Required(ErrorMessage = "The Ticket Requester Email field is required.")]
+        [EmailAddress(ErrorMessage = "Must use a valid email address.")]
+        public string TicketRequesterEmail { get; set; }
+
+        public string TicketGuid { get; set; }
+    }
+}
